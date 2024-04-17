91b03592fa5be7abe84c6a2f54a79a36b102e248
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-17 14:21:49 +0100

    windows forms app .net framework on windows

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows.Forms;
+
+public class Program
+{
+    public static Form1 form = new Form1();
+    [STAThread]
+    static void Main(string[] args)
+    {
+        form.FormLayout();;
+        Application.Run(form);
+    }
+}
