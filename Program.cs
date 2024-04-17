8e7127c87e046b2c247947d9d1f4c2d82caceed3
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-17 12:33:44 +0100

    y

--- Program.cs
+++ Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lecture1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!")
-            Console.ReadKey();
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
